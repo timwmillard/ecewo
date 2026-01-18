bcb9614890eb13f9e5ff906a9c49a5273143238c
Tim Millard <timwmillard@gmail.com>
2026-01-19 09:11:58 +1100

    better public names

--- src/utils/logger.c
+++ src/utils/logger.c
@@ -3,26 +3,26 @@
 
 #include "ecewo.h"
 
-static LogHandler ecewo_log_handler = NULL;
-static LogLevel ecewo_min_log_level = LOG_LEVEL_INFO;
+static LogHandler log_handler = NULL;
+static LogLevel min_log_level = LOG_LEVEL_INFO;
 
-void ecewo_set_log_handler(LogHandler handler) {
-    ecewo_log_handler = handler;
+void server_set_log_handler(LogHandler handler) {
+    log_handler = handler;
 }
 
-void ecewo_set_log_level(LogLevel min_level) {
-    ecewo_min_log_level = min_level;
+void server_set_log_level(LogLevel min_level) {
+    min_log_level = min_level;
 }
 
 void ecewo_log(LogLevel level, const char *file, int line,
                const char *fmt, ...) {
-    if (level < ecewo_min_log_level) return;
+    if (level < min_log_level) return;
 
     va_list args;
     va_start(args, fmt);
 
-    if (ecewo_log_handler) {
-        ecewo_log_handler(level, file, line, fmt, args);
+    if (log_handler) {
+        log_handler(level, file, line, fmt, args);
     } else {
         // Default: print to stderr
         const char *level_str[] = {"DEBUG", "INFO", "WARN", "ERROR"};
